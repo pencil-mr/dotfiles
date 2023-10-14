9f2828d145e4f0678a574323a56cdf3ed4860f2e
pencil-mr <147097627+pencil-mr@users.noreply.github.com>
2023-10-14 14:19:23 +0200

    Update dev-tools.nix
    Delete unused packages

--- dev-tools.nix
+++ dev-tools.nix
@@ -4,11 +4,6 @@ let
     unstable = import <nixos-unstable> { };
 in {
     environment.systemPackages = with pkgs; [
-    unstable.zig
-    unstable.dune_3
-    unstable.ocaml
-    unstable.ocamlPackages.ocaml-lsp
-    unstable.ocamlPackages.ocamlformat_0_25_1
     unstable.rust-analyzer
     unstable.rustc
     unstable.cargo
